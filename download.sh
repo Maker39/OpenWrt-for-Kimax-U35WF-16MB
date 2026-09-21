bf83eed0bdf8e931e9c1b469f5c48e93ecfc329b
Maker39 <47417361+Maker39@users.noreply.github.com>
2026-09-22 00:20:11 +0200

    Update download.sh

--- download.sh
+++ download.sh
@@ -6,16 +6,12 @@ TARGET="ramips"
 SUBTARGET="mt7620"
 
 IB_NAME="openwrt-imagebuilder-${VERSION}-${TARGET}-${SUBTARGET}.Linux-x86_64"
-DOWNLOAD_URL="https://downloads.openwrt.org/releases/${VERSION}/targets/${TARGET}/${SUBTARGET}/${IB_NAME}.tar.zst"
+DOWNLOAD_URL="https://openwrt.org{VERSION}/targets/${TARGET}/${SUBTARGET}/${IB_NAME}.tar.zst"
 
 echo "=== НАЧАЛО СКАЧИВАНИЯ ==="
 echo "Ссылка: ${DOWNLOAD_URL}"
 
-# Скачиваем файл с повторными попытками при сбое
 wget --tries=3 --retry-connrefused -q "$DOWNLOAD_URL"
-
-echo "Файл успешно скачан. Проверка наличия архива:"
-ls -lh *.tar.zst
 
 echo "Распаковка..."
 tar --zstd -xf "${IB_NAME}.tar.zst"
